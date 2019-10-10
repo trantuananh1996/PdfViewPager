7239d77576fda308281aa65360f8c09f71243d30
Trần Anh <trantuananh1996@gmail.com>
2019-10-10 23:54:44 +0700

    Init recyclerview

---
+++ library/src/main/java/es/voghdev/pdfviewpager/library/adapter/BasePDFRecyclerAdapter.java
@@ -0,0 +1,175 @@
+package es.voghdev.pdfviewpager.library.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.pdf.PdfRenderer;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+
+import es.voghdev.pdfviewpager.library.R;
+
+public class BasePDFRecyclerAdapter extends RecyclerView.Adapter<BasePDFRecyclerAdapter.BasePDFRecyclerViewHolder> {
+
+    protected static final int FIRST_PAGE = 0;
+    protected static final float DEFAULT_QUALITY = 2.0f;
+    protected static final int DEFAULT_OFFSCREENSIZE = 1;
+
+    protected String pdfPath;
+    protected Context context;
+    protected PdfRenderer renderer;
+    protected BitmapContainer bitmapContainer;
+    protected LayoutInflater inflater;
+
+    protected float renderQuality;
+    protected int offScreenSize;
+
+    public BasePDFRecyclerAdapter(Context context, String pdfPath) {
+        this.pdfPath = pdfPath;
+        this.context = context;
+        this.renderQuality = DEFAULT_QUALITY;
+        this.offScreenSize = DEFAULT_OFFSCREENSIZE;
+
+        init();
+    }
+
+    /**
+     * This constructor was added for those who want to customize ViewPager's offScreenSize attr
+     */
+    public BasePDFRecyclerAdapter(Context context, String pdfPath, int offScreenSize) {
+        this.pdfPath = pdfPath;
+        this.context = context;
+        this.renderQuality = DEFAULT_QUALITY;
+        this.offScreenSize = offScreenSize;
+
+        init();
+    }
+
+    @SuppressWarnings("NewApi")
+    protected void init() {
+        try {
+            renderer = new PdfRenderer(getSeekableFileDescriptor(pdfPath));
+            inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            PdfRendererParams params = extractPdfParamsFromFirstPage(renderer, renderQuality);
+            bitmapContainer = new SimpleBitmapPool(params);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("NewApi")
+    protected PdfRendererParams extractPdfParamsFromFirstPage(PdfRenderer renderer, float renderQuality) {
+        PdfRenderer.Page samplePage = getPDFPage(renderer, FIRST_PAGE);
+        PdfRendererParams params = new PdfRendererParams();
+
+        params.setRenderQuality(renderQuality);
+        params.setOffScreenSize(offScreenSize);
+        params.setWidth((int) (samplePage.getWidth() * renderQuality));
+        params.setHeight((int) (samplePage.getHeight() * renderQuality));
+
+        samplePage.close();
+
+        return params;
+    }
+
+    protected ParcelFileDescriptor getSeekableFileDescriptor(String path) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor;
+
+        File pdfCopy = new File(path);
+
+        if (pdfCopy.exists()) {
+            parcelFileDescriptor = ParcelFileDescriptor.open(pdfCopy, ParcelFileDescriptor.MODE_READ_ONLY);
+            return parcelFileDescriptor;
+        }
+
+        if (isAnAsset(path)) {
+            pdfCopy = new File(context.getCacheDir(), path);
+            parcelFileDescriptor = ParcelFileDescriptor.open(pdfCopy, ParcelFileDescriptor.MODE_READ_ONLY);
+        } else {
+            URI uri = URI.create(String.format("file://%s", path));
+            parcelFileDescriptor = context.getContentResolver().openFileDescriptor(Uri.parse(uri.toString()), "rw");
+        }
+
+        return parcelFileDescriptor;
+    }
+
+    protected boolean isAnAsset(String path) {
+        return !path.startsWith("/");
+    }
+
+
+    @SuppressWarnings("NewApi")
+    protected PdfRenderer.Page getPDFPage(PdfRenderer renderer, int position) {
+        return renderer.openPage(position);
+    }
+
+    @SuppressWarnings("NewApi")
+    public void close() {
+        releaseAllBitmaps();
+        if (renderer != null) {
+            renderer.close();
+        }
+    }
+
+    protected void releaseAllBitmaps() {
+        if (bitmapContainer != null) {
+            bitmapContainer.clear();
+        }
+    }
+
+    @NonNull
+    @Override
+    public BasePDFRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new BasePDFRecyclerViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_pdf_page, parent, false));
+    }
+
+    @Override
+    @SuppressWarnings("NewApi")
+    public void onBindViewHolder(@NonNull BasePDFRecyclerViewHolder holder, int position) {
+
+        if (renderer == null || getItemCount() < position) {
+            return;
+        }
+
+        PdfRenderer.Page page = getPDFPage(renderer, position);
+
+        Bitmap bitmap = bitmapContainer.get(position);
+        page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+        page.close();
+
+        holder.iv.setImageBitmap(bitmap);
+//        ((ViewPager) container).addView(v, 0);
+
+//        return v;
+    }
+
+    @Override
+    @SuppressWarnings("NewApi")
+    public int getItemCount() {
+        return renderer != null ? renderer.getPageCount() : 0;
+    }
+
+    class BasePDFRecyclerViewHolder extends RecyclerView.ViewHolder {
+        ImageView iv;
+        public SubsamplingScaleImageView ssiv;
+
+        public BasePDFRecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            iv = (ImageView) itemView.findViewById(R.id.imageView);
+            ssiv = itemView.findViewById(R.id.subsamplingImageView);
+        }
+    }
+}
---
+++ library/src/main/java/es/voghdev/pdfviewpager/library/adapter/PDFRecyclerAdapter.java
@@ -0,0 +1,122 @@
+package es.voghdev.pdfviewpager.library.adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.pdf.PdfRenderer;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.davemorrissey.labs.subscaleview.ImageSource;
+
+import es.voghdev.pdfviewpager.library.util.EmptyClickListener;
+
+public class PDFRecyclerAdapter extends BasePDFRecyclerAdapter {
+
+    private static final float DEFAULT_SCALE = 1f;
+
+    PdfScale scale = new PdfScale();
+    View.OnClickListener pageClickListener = new EmptyClickListener();
+
+    public PDFRecyclerAdapter(Context context, String pdfPath) {
+        super(context, pdfPath);
+    }
+
+    public PDFRecyclerAdapter(Context context, String pdfPath, int offScreenSize) {
+        super(context, pdfPath, offScreenSize);
+    }
+
+    @Override
+    @SuppressWarnings("NewApi")
+    public void onBindViewHolder(@NonNull BasePDFRecyclerViewHolder holder, int position) {
+
+        if (renderer == null || getItemCount() < position) {
+            return;
+        }
+
+        PdfRenderer.Page page = getPDFPage(renderer, position);
+
+        Bitmap bitmap = bitmapContainer.get(position);
+        holder.ssiv.setImage(ImageSource.bitmap(bitmap));
+        page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+        page.close();
+
+//        ((ViewPager) container).addView(v, 0);
+
+//        return v;
+    }
+
+    @Override
+    public void close() {
+        super.close();
+    }
+
+    public static class Builder {
+        Context context;
+        String pdfPath = "";
+        float scale = DEFAULT_SCALE;
+        float centerX = 0f, centerY = 0f;
+        int offScreenSize = DEFAULT_OFFSCREENSIZE;
+        float renderQuality = DEFAULT_QUALITY;
+        View.OnClickListener pageClickListener = new EmptyClickListener();
+
+        public Builder(Context context) {
+            this.context = context;
+        }
+
+        public Builder setScale(float scale) {
+            this.scale = scale;
+            return this;
+        }
+
+        public Builder setScale(PdfScale scale) {
+            this.scale = scale.getScale();
+            this.centerX = scale.getCenterX();
+            this.centerY = scale.getCenterY();
+            return this;
+        }
+
+        public Builder setCenterX(float centerX) {
+            this.centerX = centerX;
+            return this;
+        }
+
+        public Builder setCenterY(float centerY) {
+            this.centerY = centerY;
+            return this;
+        }
+
+        public Builder setRenderQuality(float renderQuality) {
+            this.renderQuality = renderQuality;
+            return this;
+        }
+
+        public Builder setOffScreenSize(int offScreenSize) {
+            this.offScreenSize = offScreenSize;
+            return this;
+        }
+
+        public Builder setPdfPath(String path) {
+            this.pdfPath = path;
+            return this;
+        }
+
+        public Builder setOnPageClickListener(View.OnClickListener listener) {
+            if (listener != null) {
+                pageClickListener = listener;
+            }
+            return this;
+        }
+
+        public PDFRecyclerAdapter create() {
+            PDFRecyclerAdapter adapter = new PDFRecyclerAdapter(context, pdfPath);
+            adapter.scale.setScale(scale);
+            adapter.scale.setCenterX(centerX);
+            adapter.scale.setCenterY(centerY);
+            adapter.offScreenSize = offScreenSize;
+            adapter.renderQuality = renderQuality;
+            adapter.pageClickListener = pageClickListener;
+            return adapter;
+        }
+    }
+}
